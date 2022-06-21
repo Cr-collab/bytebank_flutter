5f1be559c6cd828c49503b2d1fa8dc9e807ac9f1
Cr-collab <cristiccorrea@gmail.com>
2022-06-21 08:56:04 -0300

    MaterialApp como base da nossa aplicação. Esse widget envolverá todos os outros componentes que adicionarmos, e é responsável pelos comportamentos esperados por padrão nessa implementação, seja na parte de navegação, transição e assim por diante. Sendo assim, envolveremos basicamente todo o nosso código com o MaterialApp.
    componente Scaffold(), que nos permitirá implementarmos toda a
    estrutura básica (ou esqueleto) do Material Design.
    
    AppBar é wiget de cabecalho que fica no topo.
    
    floatingActionButton : temos que adicionar o child para termos o icone e adicionarmos
    a function onpressed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Column(
-    children: <Widget>[
-      Text(
-        "Cristiano Azevedo",
-        textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Transferencias"),
       ),
-      Text("Cristiano Azevedo - ", textDirection: TextDirection.rtl),
-      Expanded(
-        child: FittedBox(
-          fit: BoxFit.contain,
-          child: const FlutterLogo(),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
       ),
-    ],
+    ),
   ));
 }
